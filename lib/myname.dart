a55ef80f5ce41fe3d7a58633fe0dc45211dc772c
박기순 <50977788+kspark-sqisoft@users.noreply.github.com>
2024-07-17 16:52:54 +0900

    myname 추가

---
+++ lib/myname.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyName extends StatefulWidget {
+  const MyName({super.key});
+
+  @override
+  State<MyName> createState() => _MyNameState();
+}
+
+class _MyNameState extends State<MyName> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

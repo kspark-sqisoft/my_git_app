74a72bc1915c1591dfb03fcaf00e33b963cbe856
박기순 <50977788+kspark-sqisoft@users.noreply.github.com>
2024-07-17 16:24:26 +0900

    변경

--- lib/mypage.dart
+++ lib/mypage.dart
@@ -9,6 +9,12 @@ class MyPage extends StatefulWidget {
 
 class _MyPageState extends State<MyPage> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container();
   }

d2b9b44328c82a4aca5a79b0ad35da0fa5fad7a3
박기순 <50977788+kspark-sqisoft@users.noreply.github.com>
2024-07-17 16:35:49 +0900

    버전 1

---
+++ lib/mytag.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyTag extends StatefulWidget {
+  const MyTag({super.key});
+
+  @override
+  State<MyTag> createState() => _MyTagState();
+}
+
+class _MyTagState extends State<MyTag> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
